fa95b65b54510f18f4676a535f9e18597360cd86
tbossdorf <tmbossdorf@gmail.com>
2024-04-04 00:43:30 -0400

    change

--- Client.java
+++ Client.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Client {
 
 
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
         
